940b5bffe1182f89ff8528dfc8606dca44d499f6
campot05 <daniil.spectr@gmail.com>
2023-01-09 21:06:05 +0200

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,48 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 
-// Change code below this line
-
 const galleryBox = document.querySelector('.gallery');
-const galleryItem = createGaller(galleryItems);
 
-galleryBox.insertAdjacentHTML('beforeend', galleryItem);
-
-function createGaller(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `
-	 <div class="gallery__item">
-	 <a class="gallery__link" href="${original}">
-		<img
-		  class="gallery__image"
-		  src="${preview}"
-		  data-source="${original}"
-		  alt="${description}"
-		/>
-	 </a>
-  </div>`;
-    })
-    .join('');
-}
+// add gallery Items
+galleryBox.insertAdjacentHTML('beforeend', createGallery(galleryItems));
 
 galleryBox.addEventListener('click', onGalleryBoxClick);
 
+// functions //
 function onGalleryBoxClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
 
-  const largeItemUrl = getLArgeUrlItem(event);
-  console.log(largeItemUrl);
+  const urlLargeImg = event.target.dataset.source;
+  const altLArgeImg = event.target.alt;
 
-  const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`);
-
+  let instance = basicLightbox.create(
+    `<img src="${urlLargeImg}" alt="${altLArgeImg}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener('keyup', onClickEscKey);
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keyup', onClickEscKey);
+      },
+    }
+  );
   instance.show();
+
+  function onClickEscKey(event) {
+    console.log(event.code);
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
 }
 
-function getLArgeUrlItem(item) {
-  return item.target.dataset.source;
+// create galletyItems
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+		<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+		  <img
+			 class="gallery__image"
+			 src="${preview}"
+			 data-source="${original}"
+			 alt="${description}"
+		  />
+		</a>
+	 </div>`;
+    })
+    .join('');
 }
