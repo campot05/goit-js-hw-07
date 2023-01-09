3c90e9638049e105be4465604239ad0e72cfd21f
campot05 <daniil.spectr@gmail.com>
2023-01-09 21:38:21 +0200

    upgrade task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', createItemGallery(galleryItems));
+
+galleryEl.addEventListener('click', onClickGallery);
+
+function onClickGallery(event) {
+  event.preventDefault();
+}
+
+function createItemGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+		  <img
+			 class="gallery__image"
+			 src="${preview}"
+			 data-source="${original}"
+			 alt="${description}"
+		  />
+		</a>
+	 </div>`;
+    })
+    .join('');
+}
